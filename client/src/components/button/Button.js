569a706e75fbda17902853bd0737edc5df99bb1e
Jason <jason.stickel@live.com>
2018-08-17 20:25:44 -0400

    added coloring

--- client/src/components/button/Button.js
+++ client/src/components/button/Button.js
@@ -5,8 +5,22 @@ import classes from './Button.css';
 const button = props => {
     let classStyle;
 
-    if (props.display === 'clear') classStyle = classes.Clear
-    else classStyle = classes.Button
+    // if (props.display === 'clear') classStyle = classes.Clear
+    // else classStyle = classes.Button
+
+    switch(props.display) {
+        case 'clear':
+            classStyle = classes.Clear;
+            break;
+        case '+':
+        case '/':
+        case 'x':
+        case '-':
+            classStyle = classes.Maths;
+            break;
+        default:
+            classStyle = classes.Button
+    }
 
     return (
         <div className={classStyle} onClick={() => props.clicked(props.display)}>{props.display}</div>
